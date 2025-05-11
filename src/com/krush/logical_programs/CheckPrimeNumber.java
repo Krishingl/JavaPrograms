d8279d4a15d751a52e995da70c741b7e6a7f52d3
KrishIngle <krishnaingle2001@gmail.com>
2025-05-11 18:25:59 +0530

    FindPrimeNumberProgram

---
+++ src/com/krush/logical_programs/CheckPrimeNumber.java
@@ -0,0 +1,30 @@
+package com.krush.logical_programs;
+import   java.util.Scanner;
+
+public class CheckPrimeNumber {
+	
+	public static void main(String[] args) {
+		
+		  Scanner sc=new Scanner(System.in);
+		  
+		  //Ask to user Number
+		  System.out.print("Enetr the Number : ");          
+		  
+		  int number = sc.nextInt();  //Get the number to user
+		  
+		  boolean isPrime=true;
+		  
+		  
+		  for(int i = 2; i <= number/2; i++) {
+			  if(number%i==0) {
+				        isPrime=false;
+			  }
+			  
+		  }
+		
+			System.out.println("The Given Number is "+number+" "+(isPrime==true?"is Prime":"is Not Prime"));
+		
+		
+	}
+
+}
