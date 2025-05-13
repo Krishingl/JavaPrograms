8df2be44eee8a51486db104c0f9c2ac078cf6fd0
KrishIngle <krishnaingle2001@gmail.com>
2025-05-13 08:36:13 +0530

    Dog Object Created And BLC Class Ready

---
+++ src/com/krush/oop/elc/DogDemo.java
@@ -0,0 +1,14 @@
+package com.krush.oop.elc;
+import com.krush.oop.blc.Dog;
+public class DogDemo {
+	public static void main(String[] args) {
+		
+		Dog dg= new Dog("Tommy", 3, "White", 3.2);
+		
+		
+		System.out.println(dg);
+		
+		
+	}
+
+}
