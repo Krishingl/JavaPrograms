8c601fc3450dfc270a9f471366d06bca003b9bc0
KrishIngle <krishnaingle2001@gmail.com>
2025-05-22 19:05:21 +0530

    output is  10 ...20 10 ...20

---
+++ src/com/krush/mcqs/Example.java
@@ -0,0 +1,14 @@
+package com.krush.mcqs;
+
+class Example
+{
+int x=10;
+int y=20;
+public static void main(String[] args)
+{
+Example e1=new Example();
+Example e2=new Example();
+System.out.println(e1.x+" ..."+e1.y); //10 ...20
+System.out.println(e2.x+" ..."+e2.y);  //20 ...20
+}
+}
