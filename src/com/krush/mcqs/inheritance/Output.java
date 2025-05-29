57313821f2cf37563591dc6b92d99cee2ee12802
KrishIngle <krishnaingle2001@gmail.com>
2025-05-29 19:12:15 +0530

    i is zero. i is one. i is two. i is three. i is greater than three. i is greater than three.

--- src/com/krush/mcqs/inheritance/Output.java
+++ src/com/krush/mcqs/inheritance/Output.java
@@ -96,35 +96,95 @@ package com.krush.mcqs.inheritance;
 //}
 ////Output =  0 1 2 3 4 5
 
+//
+//class Output
+//{
+//    public static void main(String [] args)
+//    {
+//        Count2 count = new Count2();
+//        count.count();
+//    }
+//}
+//
+//class Count extends Count2
+//{
+//    Count()
+//    {
+//
+//    }
+//}
+//class Count2
+//{
+//    void count()
+//    {
+//        int i = 5;
+//        System.out.print("Output = ");
+//
+//        for (int x = 0; x <= (i % 7); x += 1)
+//        {
+//            System.out.print(" " + x);
+//            i++;
+//        }
+//    }
+//}
+//
+//
 
-class Output
+
+
+//Que-6)
+//___________________
+//What will be the output of the following program?
+
+public class Output
 {
-    public static void main(String [] args)
+    public static void main(String s[])
     {
-        Count2 count = new Count2();
-        count.count();
+        int i = 2;
+        B b = new B();
+        b.execute();
     }
 }
 
-class Count extends Count2
+class A
 {
-    Count()
-    {
+    int i = 0;
+    A(){}
+}
 
+class B extends A
+{
+    void execute()
+    {
+        for(; i < 6; i++)
+        
+            switch(i)
+            {
+                case 0:
+                System.out.println("i is zero.");
+                break;
+        
+                case 1:
+                System.out.println("i is one.");
+                break;
+        
+                case 2:
+                System.out.println("i is two.");
+                continue;
+        
+                case 3:
+                System.out.println("i is three.");
+                break;
+        
+                default:
+                System.out.println("i is greater than three.");
+            }
     }
 }
-class Count2
-{
-    void count()
-    {
-        int i = 5;
-        System.out.print("Output = ");
-
-        for (int x = 0; x <= (i % 7); x += 1)
-        {
-            System.out.print(" " + x);
-            i++;
-        }
-    }
-}
-//Output =  0 1
+/*i is zero.
+i is one.
+i is two.
+i is three.
+i is greater than three.
+i is greater than three.
+*/
