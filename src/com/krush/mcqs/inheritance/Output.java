dc9a467c49f9dda1ef98ea12050126449a267695
KrishIngle <krishnaingle2001@gmail.com>
2025-05-29 18:59:18 +0530

    Output =  0 2 4 6

--- src/com/krush/mcqs/inheritance/Output.java
+++ src/com/krush/mcqs/inheritance/Output.java
@@ -1,26 +1,58 @@
 package com.krush.mcqs.inheritance;
+//
+//public class Output
+//{
+//    public static void main(String[] args)
+//    {
+//        new Alpha();  //
+//        new Base();
+//    }
+//}
+//
+//class Alpha extends Base
+//{
+//    Alpha()
+//    {    //Super();
+//        System.out.print("Base");
+//    }
+//}
+//
+//class Base
+//{  
+//    Base()
+//    {   //super();
+//        System.out.println("Base");
+//    }
+//}
+
+
+
 
 public class Output
 {
-    public static void main(String[] args)
+    public static void main(String [] args)
     {
-        new Alpha();  //
-        new Base();
+        Count t = new Count();
+        t.count();
     }
 }
 
-class Alpha extends Base
+class Count extends Count2
 {
-    Alpha()
-    {    //Super();
-        System.out.print("Base");
+    Count()
+    { //super();
+
     }
 }
+class Count2
+{
+    void count()
+    {
+        System.out.print("Output = ");
 
-class Base
-{  
-    Base()
-    {   //super();
-        System.out.println("Base");
+        for (int x = 0; x < 7; x++,x++ )
+        {
+            System.out.print(" " + x);
+        }
     }
 }
